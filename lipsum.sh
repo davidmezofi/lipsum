cc1b1123dc3ca81234c82505805ac1ef5b5669d3
Dávid Mezőfi <david.mezofi93@gmail.com>
2021-12-10 20:57:57 +0100

    Initialize repo

---
+++ lipsum.sh
@@ -0,0 +1,325 @@
+#! /bin/bash
+
+# Skip the following 210 lines
+WORDS[0]="a"
+WORDS[1]="ab"
+WORDS[2]="accusamus"
+WORDS[3]="accusantium"
+WORDS[4]="ad"
+WORDS[5]="adipisci"
+WORDS[6]="adipiscing"
+WORDS[7]="alias"
+WORDS[8]="aliqua"
+WORDS[9]="aliquam"
+WORDS[10]="aliquid"
+WORDS[11]="aliquip"
+WORDS[12]="amet"
+WORDS[13]="anim"
+WORDS[14]="animi"
+WORDS[15]="aperiam"
+WORDS[16]="architecto"
+WORDS[17]="asperiores"
+WORDS[18]="aspernatur"
+WORDS[19]="assumenda"
+WORDS[20]="at"
+WORDS[21]="atque"
+WORDS[22]="aut"
+WORDS[23]="aute"
+WORDS[24]="autem"
+WORDS[25]="beatae"
+WORDS[26]="blanditiis"
+WORDS[27]="cillum"
+WORDS[28]="commodi"
+WORDS[29]="commodo"
+WORDS[30]="consectetur"
+WORDS[31]="consequat"
+WORDS[32]="consequatur"
+WORDS[33]="consequuntur"
+WORDS[34]="corporis"
+WORDS[35]="corrupti"
+WORDS[36]="culpa"
+WORDS[37]="cum"
+WORDS[38]="cumque"
+WORDS[39]="cupidatat"
+WORDS[40]="cupiditate"
+WORDS[41]="debitis"
+WORDS[42]="delectus"
+WORDS[43]="deleniti"
+WORDS[44]="deserunt"
+WORDS[45]="dicta"
+WORDS[46]="dignissimos"
+WORDS[47]="distinctio"
+WORDS[48]="do"
+WORDS[49]="dolor"
+WORDS[50]="dolore"
+WORDS[51]="dolorem"
+WORDS[52]="doloremque"
+WORDS[53]="dolores"
+WORDS[54]="doloribus"
+WORDS[55]="dolorum"
+WORDS[56]="ducimus"
+WORDS[57]="duis"
+WORDS[58]="ea"
+WORDS[59]="eaque"
+WORDS[60]="earum"
+WORDS[61]="eius"
+WORDS[62]="eiusmod"
+WORDS[63]="eligendi"
+WORDS[64]="elit"
+WORDS[65]="enim"
+WORDS[66]="eos"
+WORDS[67]="error"
+WORDS[68]="esse"
+WORDS[69]="est"
+WORDS[70]="et"
+WORDS[71]="eu"
+WORDS[72]="eum"
+WORDS[73]="eveniet"
+WORDS[74]="ex"
+WORDS[75]="excepteur"
+WORDS[76]="excepturi"
+WORDS[77]="exercitation"
+WORDS[78]="exercitationem"
+WORDS[79]="expedita"
+WORDS[80]="explicabo"
+WORDS[81]="facere"
+WORDS[82]="facilis"
+WORDS[83]="fuga"
+WORDS[84]="fugiat"
+WORDS[85]="fugit"
+WORDS[86]="harum"
+WORDS[87]="hic"
+WORDS[88]="id"
+WORDS[89]="illo"
+WORDS[90]="illum"
+WORDS[91]="impedit"
+WORDS[92]="in"
+WORDS[93]="incididunt"
+WORDS[94]="incidunt"
+WORDS[95]="inventore"
+WORDS[96]="ipsa"
+WORDS[97]="ipsam"
+WORDS[98]="ipsum"
+WORDS[99]="irure"
+WORDS[100]="iste"
+WORDS[101]="itaque"
+WORDS[102]="iure"
+WORDS[103]="iusto"
+WORDS[104]="labore"
+WORDS[105]="laboriosam"
+WORDS[106]="laboris"
+WORDS[107]="laborum"
+WORDS[108]="laudantium"
+WORDS[109]="libero"
+WORDS[110]="lorem"
+WORDS[111]="magna"
+WORDS[112]="magnam"
+WORDS[113]="magni"
+WORDS[114]="maiores"
+WORDS[115]="maxime"
+WORDS[116]="minim"
+WORDS[117]="minima"
+WORDS[118]="minus"
+WORDS[119]="modi"
+WORDS[120]="molestiae"
+WORDS[121]="molestias"
+WORDS[122]="mollit"
+WORDS[123]="mollitia"
+WORDS[124]="nam"
+WORDS[125]="natus"
+WORDS[126]="necessitatibus"
+WORDS[127]="nemo"
+WORDS[128]="neque"
+WORDS[129]="nesciunt"
+WORDS[130]="nihil"
+WORDS[131]="nisi"
+WORDS[132]="nobis"
+WORDS[133]="non"
+WORDS[134]="nostrud"
+WORDS[135]="nostrum"
+WORDS[136]="nulla"
+WORDS[137]="numquam"
+WORDS[138]="occaecat"
+WORDS[139]="occaecati"
+WORDS[140]="odio"
+WORDS[141]="odit"
+WORDS[142]="officia"
+WORDS[143]="officiis"
+WORDS[144]="omnis"
+WORDS[145]="optio"
+WORDS[146]="pariatur"
+WORDS[147]="perferendis"
+WORDS[148]="perspiciatis"
+WORDS[149]="placeat"
+WORDS[150]="porro"
+WORDS[151]="possimus"
+WORDS[152]="praesentium"
+WORDS[153]="proident"
+WORDS[154]="provident"
+WORDS[155]="quae"
+WORDS[156]="quaerat"
+WORDS[157]="quam"
+WORDS[158]="quas"
+WORDS[159]="quasi"
+WORDS[160]="qui"
+WORDS[161]="quia"
+WORDS[162]="quibusdam"
+WORDS[163]="quidem"
+WORDS[164]="quis"
+WORDS[165]="quisquam"
+WORDS[166]="quo"
+WORDS[167]="quod"
+WORDS[168]="quos"
+WORDS[169]="ratione"
+WORDS[170]="recusandae"
+WORDS[171]="reiciendis"
+WORDS[172]="rem"
+WORDS[173]="repellat"
+WORDS[174]="repellendus"
+WORDS[175]="reprehenderit"
+WORDS[176]="repudiandae"
+WORDS[177]="rerum"
+WORDS[178]="saepe"
+WORDS[179]="sapiente"
+WORDS[180]="sed"
+WORDS[181]="sequi"
+WORDS[182]="similique"
+WORDS[183]="sint"
+WORDS[184]="sit"
+WORDS[185]="soluta"
+WORDS[186]="sunt"
+WORDS[187]="suscipit"
+WORDS[188]="tempor"
+WORDS[189]="tempora"
+WORDS[190]="tempore"
+WORDS[191]="temporibus"
+WORDS[192]="tenetur"
+WORDS[193]="totam"
+WORDS[194]="ullam"
+WORDS[195]="ullamco"
+WORDS[196]="unde"
+WORDS[197]="ut"
+WORDS[198]="vel"
+WORDS[199]="velit"
+WORDS[200]="veniam"
+WORDS[201]="veritatis"
+WORDS[202]="vero"
+WORDS[203]="vitae"
+WORDS[204]="voluptas"
+WORDS[205]="voluptate"
+WORDS[206]="voluptatem"
+WORDS[207]="voluptates"
+WORDS[208]="voluptatibus"
+WORDS[209]="voluptatum"
+
+printlipsum() {
+    cat << EOF
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+culpa qui officia deserunt mollit anim id est laborum.
+EOF
+}
+
+prettify() {
+    tr '\n' ' ' | sed -e "s/ \{2,\}/ /g" | fold -s
+    echo
+}
+
+toupper() {
+    FIRSTC=$(echo "$1" | cut -c 1 | tr '[:lower:]' '[:upper:]') 
+    REST=$(echo "$1" | cut -c 2-)
+    printf "%s%s" "$FIRSTC" "$REST"
+}
+
+punctuation() {
+    case "$1" in
+        0 | 1 | 2 | 3 | 4 | 5) printf ". "
+            ;;
+        6) printf "? "
+            ;;
+    esac
+}
+
+myrandom() {
+    head -c 1 /dev/urandom | od -A n -d | sed -e "s/ \+//g"
+}
+
+printrandompar() {
+    # In the original paragraphs there are 4 or 5 sentences
+    NSENTENCE=$((4 + RANDOM % 2))
+    while [ "$NSENTENCE" -gt 0 ]
+    do
+        # In the original sentences there are 8-29 words. (The 40 word sentence
+        # is an outlier, check with a boxplot.)
+        NWORDS=$((8 + RANDOM % 22))
+        toupper "${WORDS[$((RANDOM % 210))]}"
+        while [ "$NWORDS" -gt 0 ]
+        do
+            printf " %s" "${WORDS[$((RANDOM % 210))]}"
+            NWORDS=$((NWORDS - 1))
+        done
+        punctuation "$((RANDOM % 7))"
+        NSENTENCE=$((NSENTENCE - 1))
+    done
+}
+
+printpars() {
+    NPAR=$(($1 - 1))
+
+    printlipsum | prettify
+
+    while [ "$NPAR" -gt 0 ]
+    do
+        echo
+        printrandompar | prettify
+        NPAR=$((NPAR - 1))
+    done
+}
+
+SCRIPTNAME=$(basename "$0")
+
+usage() {
+    echo "Usage: $SCRIPTNAME [ -p POSINT ]" >&2
+}
+
+# If there was no argument given, then just printlipsum and exit
+[ -z "$*" ] && printlipsum | prettify && exit 0
+
+# We need it to be set, otherwise later the checks will complain
+NPAR=0
+
+# The following option handling comes from getopt-example.bash
+TEMP=$(getopt -o 'p:' -n "$SCRIPTNAME" -- "$@")
+eval set -- "$TEMP"
+unset TEMP
+
+while true
+do
+    case "$1" in
+        '-p')
+            echo "$2" | grep -q -v "[1-9][0-9]*" \
+                && echo "$SCRIPTNAME: -p requires a positive number as an argument" >&2\
+                && exit 1
+            NPAR=$2
+            shift 2
+            continue
+        ;;
+        '--')
+            shift
+            break
+        ;;
+        *)
+            usage
+            exit 3
+        ;;
+    esac
+done
+
+[ -n "$*" ] && echo "$SCRIPTNAME: too many arguments" >&2 && usage && exit 2
+[ "$NPAR" -eq 1 ] && printlipsum | prettify && exit 0
+[ "$NPAR" -gt 1 ] && printpars "$NPAR" && exit 0
+
+usage && exit 3
---
+++ setup.sh
@@ -0,0 +1,60 @@
+#! /bin/sh
+
+# List all the distinct words (ignore cases) in the files
+distinctwords() {
+    cat lipsum.txt definibus.1.10.32.txt definibus.1.10.33.txt | \
+        tr ' ' '\n' | tr -d '.,!?' | tr '[:upper:]' '[:lower:]' | sort | \
+        uniq 
+}
+
+# Print the array used in lipsum.sh
+wordsarray() {
+    NLINE=0
+    distinctwords | {
+        while read -r LINE
+        do
+            echo "WORDS[$NLINE]=\"$LINE\""
+            NLINE=$((NLINE + 1))
+        done
+    }
+}
+
+breaksentences() {
+    tr -d '\n' | sed -e "s/\([.!?]\) */\1\n/g"
+}
+
+nsentences() {
+    printf "%s : " "$1"
+    breaksentences < "$1" | wc -l
+}
+
+printf "FILE : NSENTENCES\n"
+nsentences lipsum.txt
+nsentences definibus.1.10.32.txt
+nsentences definibus.1.10.33.txt
+echo
+
+punctmark() {
+    printf "%s\t\t" "$1"
+    cat lipsum.txt definibus.1.10.32.txt definibus.1.10.33.txt | grep -c "\\$1"
+}
+
+printf "PUNCTMARK\tNSENTENCES\n"
+punctmark '.'
+punctmark '?'
+punctmark '!'
+echo
+
+sentencelens() {
+    breaksentences | {
+    while read -r LINE
+    do
+        echo "$LINE" | wc -w
+    done
+    }
+}
+
+echo "Number of words pro sentence:"
+cat lipsum.txt definibus.1.10.32.txt definibus.1.10.33.txt | sentencelens | \
+    sort -n | tr '\n' ' '
+echo
