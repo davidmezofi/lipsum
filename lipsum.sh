c963047d846f436a779c71cda076678a9b134a3e
Dávid Mezőfi <david.mezofi93@gmail.com>
2021-12-13 20:32:52 +0100

    Use function instead of an array

--- lipsum.sh
+++ lipsum.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 
 # Copyright 2021 Dávid Csaba Mezőfi
 
@@ -14,216 +14,220 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-WORDS[0]="a"
-WORDS[1]="ab"
-WORDS[2]="accusamus"
-WORDS[3]="accusantium"
-WORDS[4]="ad"
-WORDS[5]="adipisci"
-WORDS[6]="adipiscing"
-WORDS[7]="alias"
-WORDS[8]="aliqua"
-WORDS[9]="aliquam"
-WORDS[10]="aliquid"
-WORDS[11]="aliquip"
-WORDS[12]="amet"
-WORDS[13]="anim"
-WORDS[14]="animi"
-WORDS[15]="aperiam"
-WORDS[16]="architecto"
-WORDS[17]="asperiores"
-WORDS[18]="aspernatur"
-WORDS[19]="assumenda"
-WORDS[20]="at"
-WORDS[21]="atque"
-WORDS[22]="aut"
-WORDS[23]="aute"
-WORDS[24]="autem"
-WORDS[25]="beatae"
-WORDS[26]="blanditiis"
-WORDS[27]="cillum"
-WORDS[28]="commodi"
-WORDS[29]="commodo"
-WORDS[30]="consectetur"
-WORDS[31]="consequat"
-WORDS[32]="consequatur"
-WORDS[33]="consequuntur"
-WORDS[34]="corporis"
-WORDS[35]="corrupti"
-WORDS[36]="culpa"
-WORDS[37]="cum"
-WORDS[38]="cumque"
-WORDS[39]="cupidatat"
-WORDS[40]="cupiditate"
-WORDS[41]="debitis"
-WORDS[42]="delectus"
-WORDS[43]="deleniti"
-WORDS[44]="deserunt"
-WORDS[45]="dicta"
-WORDS[46]="dignissimos"
-WORDS[47]="distinctio"
-WORDS[48]="do"
-WORDS[49]="dolor"
-WORDS[50]="dolore"
-WORDS[51]="dolorem"
-WORDS[52]="doloremque"
-WORDS[53]="dolores"
-WORDS[54]="doloribus"
-WORDS[55]="dolorum"
-WORDS[56]="ducimus"
-WORDS[57]="duis"
-WORDS[58]="ea"
-WORDS[59]="eaque"
-WORDS[60]="earum"
-WORDS[61]="eius"
-WORDS[62]="eiusmod"
-WORDS[63]="eligendi"
-WORDS[64]="elit"
-WORDS[65]="enim"
-WORDS[66]="eos"
-WORDS[67]="error"
-WORDS[68]="esse"
-WORDS[69]="est"
-WORDS[70]="et"
-WORDS[71]="eu"
-WORDS[72]="eum"
-WORDS[73]="eveniet"
-WORDS[74]="ex"
-WORDS[75]="excepteur"
-WORDS[76]="excepturi"
-WORDS[77]="exercitation"
-WORDS[78]="exercitationem"
-WORDS[79]="expedita"
-WORDS[80]="explicabo"
-WORDS[81]="facere"
-WORDS[82]="facilis"
-WORDS[83]="fuga"
-WORDS[84]="fugiat"
-WORDS[85]="fugit"
-WORDS[86]="harum"
-WORDS[87]="hic"
-WORDS[88]="id"
-WORDS[89]="illo"
-WORDS[90]="illum"
-WORDS[91]="impedit"
-WORDS[92]="in"
-WORDS[93]="incididunt"
-WORDS[94]="incidunt"
-WORDS[95]="inventore"
-WORDS[96]="ipsa"
-WORDS[97]="ipsam"
-WORDS[98]="ipsum"
-WORDS[99]="irure"
-WORDS[100]="iste"
-WORDS[101]="itaque"
-WORDS[102]="iure"
-WORDS[103]="iusto"
-WORDS[104]="labore"
-WORDS[105]="laboriosam"
-WORDS[106]="laboris"
-WORDS[107]="laborum"
-WORDS[108]="laudantium"
-WORDS[109]="libero"
-WORDS[110]="lorem"
-WORDS[111]="magna"
-WORDS[112]="magnam"
-WORDS[113]="magni"
-WORDS[114]="maiores"
-WORDS[115]="maxime"
-WORDS[116]="minim"
-WORDS[117]="minima"
-WORDS[118]="minus"
-WORDS[119]="modi"
-WORDS[120]="molestiae"
-WORDS[121]="molestias"
-WORDS[122]="mollit"
-WORDS[123]="mollitia"
-WORDS[124]="nam"
-WORDS[125]="natus"
-WORDS[126]="necessitatibus"
-WORDS[127]="nemo"
-WORDS[128]="neque"
-WORDS[129]="nesciunt"
-WORDS[130]="nihil"
-WORDS[131]="nisi"
-WORDS[132]="nobis"
-WORDS[133]="non"
-WORDS[134]="nostrud"
-WORDS[135]="nostrum"
-WORDS[136]="nulla"
-WORDS[137]="numquam"
-WORDS[138]="occaecat"
-WORDS[139]="occaecati"
-WORDS[140]="odio"
-WORDS[141]="odit"
-WORDS[142]="officia"
-WORDS[143]="officiis"
-WORDS[144]="omnis"
-WORDS[145]="optio"
-WORDS[146]="pariatur"
-WORDS[147]="perferendis"
-WORDS[148]="perspiciatis"
-WORDS[149]="placeat"
-WORDS[150]="porro"
-WORDS[151]="possimus"
-WORDS[152]="praesentium"
-WORDS[153]="proident"
-WORDS[154]="provident"
-WORDS[155]="quae"
-WORDS[156]="quaerat"
-WORDS[157]="quam"
-WORDS[158]="quas"
-WORDS[159]="quasi"
-WORDS[160]="qui"
-WORDS[161]="quia"
-WORDS[162]="quibusdam"
-WORDS[163]="quidem"
-WORDS[164]="quis"
-WORDS[165]="quisquam"
-WORDS[166]="quo"
-WORDS[167]="quod"
-WORDS[168]="quos"
-WORDS[169]="ratione"
-WORDS[170]="recusandae"
-WORDS[171]="reiciendis"
-WORDS[172]="rem"
-WORDS[173]="repellat"
-WORDS[174]="repellendus"
-WORDS[175]="reprehenderit"
-WORDS[176]="repudiandae"
-WORDS[177]="rerum"
-WORDS[178]="saepe"
-WORDS[179]="sapiente"
-WORDS[180]="sed"
-WORDS[181]="sequi"
-WORDS[182]="similique"
-WORDS[183]="sint"
-WORDS[184]="sit"
-WORDS[185]="soluta"
-WORDS[186]="sunt"
-WORDS[187]="suscipit"
-WORDS[188]="tempor"
-WORDS[189]="tempora"
-WORDS[190]="tempore"
-WORDS[191]="temporibus"
-WORDS[192]="tenetur"
-WORDS[193]="totam"
-WORDS[194]="ullam"
-WORDS[195]="ullamco"
-WORDS[196]="unde"
-WORDS[197]="ut"
-WORDS[198]="vel"
-WORDS[199]="velit"
-WORDS[200]="veniam"
-WORDS[201]="veritatis"
-WORDS[202]="vero"
-WORDS[203]="vitae"
-WORDS[204]="voluptas"
-WORDS[205]="voluptate"
-WORDS[206]="voluptatem"
-WORDS[207]="voluptates"
-WORDS[208]="voluptatibus"
-WORDS[209]="voluptatum"
+word() {
+    case "$1" in
+        0) printf "a";;
+        1) printf "ab";;
+        2) printf "accusamus";;
+        3) printf "accusantium";;
+        4) printf "ad";;
+        5) printf "adipisci";;
+        6) printf "adipiscing";;
+        7) printf "alias";;
+        8) printf "aliqua";;
+        9) printf "aliquam";;
+        10) printf "aliquid";;
+        11) printf "aliquip";;
+        12) printf "amet";;
+        13) printf "anim";;
+        14) printf "animi";;
+        15) printf "aperiam";;
+        16) printf "architecto";;
+        17) printf "asperiores";;
+        18) printf "aspernatur";;
+        19) printf "assumenda";;
+        20) printf "at";;
+        21) printf "atque";;
+        22) printf "aut";;
+        23) printf "aute";;
+        24) printf "autem";;
+        25) printf "beatae";;
+        26) printf "blanditiis";;
+        27) printf "cillum";;
+        28) printf "commodi";;
+        29) printf "commodo";;
+        30) printf "consectetur";;
+        31) printf "consequat";;
+        32) printf "consequatur";;
+        33) printf "consequuntur";;
+        34) printf "corporis";;
+        35) printf "corrupti";;
+        36) printf "culpa";;
+        37) printf "cum";;
+        38) printf "cumque";;
+        39) printf "cupidatat";;
+        40) printf "cupiditate";;
+        41) printf "debitis";;
+        42) printf "delectus";;
+        43) printf "deleniti";;
+        44) printf "deserunt";;
+        45) printf "dicta";;
+        46) printf "dignissimos";;
+        47) printf "distinctio";;
+        48) printf "do";;
+        49) printf "dolor";;
+        50) printf "dolore";;
+        51) printf "dolorem";;
+        52) printf "doloremque";;
+        53) printf "dolores";;
+        54) printf "doloribus";;
+        55) printf "dolorum";;
+        56) printf "ducimus";;
+        57) printf "duis";;
+        58) printf "ea";;
+        59) printf "eaque";;
+        60) printf "earum";;
+        61) printf "eius";;
+        62) printf "eiusmod";;
+        63) printf "eligendi";;
+        64) printf "elit";;
+        65) printf "enim";;
+        66) printf "eos";;
+        67) printf "error";;
+        68) printf "esse";;
+        69) printf "est";;
+        70) printf "et";;
+        71) printf "eu";;
+        72) printf "eum";;
+        73) printf "eveniet";;
+        74) printf "ex";;
+        75) printf "excepteur";;
+        76) printf "excepturi";;
+        77) printf "exercitation";;
+        78) printf "exercitationem";;
+        79) printf "expedita";;
+        80) printf "explicabo";;
+        81) printf "facere";;
+        82) printf "facilis";;
+        83) printf "fuga";;
+        84) printf "fugiat";;
+        85) printf "fugit";;
+        86) printf "harum";;
+        87) printf "hic";;
+        88) printf "id";;
+        89) printf "illo";;
+        90) printf "illum";;
+        91) printf "impedit";;
+        92) printf "in";;
+        93) printf "incididunt";;
+        94) printf "incidunt";;
+        95) printf "inventore";;
+        96) printf "ipsa";;
+        97) printf "ipsam";;
+        98) printf "ipsum";;
+        99) printf "irure";;
+        100) printf "iste";;
+        101) printf "itaque";;
+        102) printf "iure";;
+        103) printf "iusto";;
+        104) printf "labore";;
+        105) printf "laboriosam";;
+        106) printf "laboris";;
+        107) printf "laborum";;
+        108) printf "laudantium";;
+        109) printf "libero";;
+        110) printf "lorem";;
+        111) printf "magna";;
+        112) printf "magnam";;
+        113) printf "magni";;
+        114) printf "maiores";;
+        115) printf "maxime";;
+        116) printf "minim";;
+        117) printf "minima";;
+        118) printf "minus";;
+        119) printf "modi";;
+        120) printf "molestiae";;
+        121) printf "molestias";;
+        122) printf "mollit";;
+        123) printf "mollitia";;
+        124) printf "nam";;
+        125) printf "natus";;
+        126) printf "necessitatibus";;
+        127) printf "nemo";;
+        128) printf "neque";;
+        129) printf "nesciunt";;
+        130) printf "nihil";;
+        131) printf "nisi";;
+        132) printf "nobis";;
+        133) printf "non";;
+        134) printf "nostrud";;
+        135) printf "nostrum";;
+        136) printf "nulla";;
+        137) printf "numquam";;
+        138) printf "occaecat";;
+        139) printf "occaecati";;
+        140) printf "odio";;
+        141) printf "odit";;
+        142) printf "officia";;
+        143) printf "officiis";;
+        144) printf "omnis";;
+        145) printf "optio";;
+        146) printf "pariatur";;
+        147) printf "perferendis";;
+        148) printf "perspiciatis";;
+        149) printf "placeat";;
+        150) printf "porro";;
+        151) printf "possimus";;
+        152) printf "praesentium";;
+        153) printf "proident";;
+        154) printf "provident";;
+        155) printf "quae";;
+        156) printf "quaerat";;
+        157) printf "quam";;
+        158) printf "quas";;
+        159) printf "quasi";;
+        160) printf "qui";;
+        161) printf "quia";;
+        162) printf "quibusdam";;
+        163) printf "quidem";;
+        164) printf "quis";;
+        165) printf "quisquam";;
+        166) printf "quo";;
+        167) printf "quod";;
+        168) printf "quos";;
+        169) printf "ratione";;
+        170) printf "recusandae";;
+        171) printf "reiciendis";;
+        172) printf "rem";;
+        173) printf "repellat";;
+        174) printf "repellendus";;
+        175) printf "reprehenderit";;
+        176) printf "repudiandae";;
+        177) printf "rerum";;
+        178) printf "saepe";;
+        179) printf "sapiente";;
+        180) printf "sed";;
+        181) printf "sequi";;
+        182) printf "similique";;
+        183) printf "sint";;
+        184) printf "sit";;
+        185) printf "soluta";;
+        186) printf "sunt";;
+        187) printf "suscipit";;
+        188) printf "tempor";;
+        189) printf "tempora";;
+        190) printf "tempore";;
+        191) printf "temporibus";;
+        192) printf "tenetur";;
+        193) printf "totam";;
+        194) printf "ullam";;
+        195) printf "ullamco";;
+        196) printf "unde";;
+        197) printf "ut";;
+        198) printf "vel";;
+        199) printf "velit";;
+        200) printf "veniam";;
+        201) printf "veritatis";;
+        202) printf "vero";;
+        203) printf "vitae";;
+        204) printf "voluptas";;
+        205) printf "voluptate";;
+        206) printf "voluptatem";;
+        207) printf "voluptates";;
+        208) printf "voluptatibus";;
+        209) printf "voluptatum";;
+    esac
+}
 
 printlipsum() {
     cat << EOF
@@ -268,10 +272,10 @@ printrandompar() {
         # In the original sentences there are 8-29 words. (The 40 word sentence
         # is an outlier, check with a boxplot.)
         NWORDS=$((8 + RANDOM % 22))
-        toupper "${WORDS[$((RANDOM % 210))]}"
+        toupper "$(word $((RANDOM % 210)))"
         while [ "$NWORDS" -gt 0 ]
         do
-            printf " %s" "${WORDS[$((RANDOM % 210))]}"
+            printf " %s" "$(word $((RANDOM % 210)))"
             NWORDS=$((NWORDS - 1))
         done
         punctuation "$((RANDOM % 7))"
